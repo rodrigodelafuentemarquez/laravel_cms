4eca103a606ae07ecda9699d26226f7e3f05ad0a
Rodrigo <rodrigo@blacknosaur.com>
2018-02-16 15:27:39 +0100

    Users funcionando con todo pero sin seguridad

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('prueba', function() {
+    return view('prueba');
+});
+
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
